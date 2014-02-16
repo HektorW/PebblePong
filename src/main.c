2daab1fead706e2961e6d59969758f1c87abbfe9
Hektor Wallin <hektorw@gmail.com>
2014-02-16 12:53:23 -0800

    spinning cube, with button input

--- src/main.c
+++ src/main.c
@@ -1,21 +1,114 @@
-#include <pebble.h>
+#include "pebble.h"
 
-Window *my_window;
-TextLayer *text_layer;
+static Window *window;
+static Layer *square_layer;
 
-void handle_init(void) {
-	  my_window = window_create();
+// Timers can be canceled with `app_timer_cancel()`
+static AppTimer *timer;
 
-	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
-}
 
-void handle_deinit(void) {
-	  text_layer_destroy(text_layer);
-	  window_destroy(my_window);
-}
+// functions
+static void init();
+static void deinit();
+static void update_square_layer(Layer*, GContext*);
+static void timer_callback(void*);
+static void click_config_provider(Window*);
+
+static void button_up_handler(ClickRecognizerRef, void*);
+
+
+// square points
+static const GPathInfo SQUARE_POINTS = {
+  4,
+  (GPoint []) {
+    {-10, -10},
+    {-10,  10},
+    { 10,  10},
+    { 10, -10}
+  }
+};
+
+static GPath *square_path;
+
+static int multiplier = 1;
+
+
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+  init();
+
+  app_event_loop();
+
+  deinit();
 }
+
+
+static void update_square_layer(Layer *layer, GContext* ctx) {
+  static unsigned int angle = 0;
+
+  gpath_rotate_to(square_path, (TRIG_MAX_ANGLE / 360) * angle);
+
+  angle = (angle + (5 * multiplier)) % 360;
+
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  gpath_draw_outline(ctx, square_path);
+}
+
+
+static void timer_callback(void *context) {
+  layer_mark_dirty(square_layer);
+
+  const uint32_t timeout_ms = 50;
+  timer = app_timer_register(timeout_ms, timer_callback, NULL);
+}
+
+
+static void click_config_provider(Window *window) {
+  window_raw_click_subscribe(BUTTON_ID_UP, NULL, button_up_handler, NULL);
+  window_raw_click_subscribe(BUTTON_ID_DOWN, NULL, button_up_handler, NULL);
+  window_raw_click_subscribe(BUTTON_ID_SELECT, NULL, button_up_handler, NULL);
+}
+
+
+
+static void button_up_handler(ClickRecognizerRef recognizer, void *context) {
+  ButtonId button_id = click_recognizer_get_button_id(recognizer);
+  if(button_id == BUTTON_ID_UP)
+    multiplier -= 1;
+  else if(button_id == BUTTON_ID_DOWN)
+    multiplier += 1;
+  else if(button_id == BUTTON_ID_SELECT)
+    multiplier = 0;
+}
+
+
+
+static void init(void) {
+  window = window_create();
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  square_layer = layer_create(bounds);
+  layer_set_update_proc(square_layer, update_square_layer);
+  layer_add_child(window_layer, square_layer);
+
+  square_path = gpath_create(&SQUARE_POINTS);
+  gpath_move_to(square_path, grect_center_point(&bounds));
+
+  const bool animated = true;
+  window_stack_push(window, animated);
+
+  const uint32_t timeout_ms = 50;
+  timer = app_timer_register(timeout_ms, timer_callback, NULL);
+
+  window_set_click_config_provider(window, (ClickConfigProvider)click_config_provider);
+}
+
+static void deinit(void) {
+  gpath_destroy(square_path);
+
+  layer_destroy(square_layer);
+  window_destroy(window);
+}
+
